a6ab63536144fcd72994bcebc0b7efc7547bc702
Li Zhenghua <504524274@qq.com>
2016-05-14 22:31:20 +0800

    update database structure

--- meiya.sql
+++ meiya.sql
@@ -1,33 +1,36 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50505
-Source Host           : localhost:3306
-Source Database       : meiya
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50616
+ Source Host           : localhost
+ Source Database       : meiya
 
-Target Server Type    : MYSQL
-Target Server Version : 50505
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50616
+ File Encoding         : utf-8
 
-Date: 2016-05-06 18:51:06
+ Date: 05/14/2016 22:30:29 PM
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for pics
+--  Table structure for `pics`
 -- ----------------------------
 DROP TABLE IF EXISTS `pics`;
 CREATE TABLE `pics` (
-  `pid` int(11) NOT NULL AUTO_INCREMENT,
-  `url` varchar(255) DEFAULT NULL,
-  `visible` int(1) DEFAULT NULL,
-  PRIMARY KEY (`pid`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `pid` varchar(255) NOT NULL,
+  `visible` int(1) NOT NULL,
+  `addTime` int(16) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for user
+--  Table structure for `user`
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
@@ -37,3 +40,5 @@ CREATE TABLE `user` (
   `authority` int(255) DEFAULT NULL,
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
