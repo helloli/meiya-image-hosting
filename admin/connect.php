6f37088d8961f4f559417ff2525dbf68754f8e82
lizhenghua <lizhenghua@360.cn>
2016-05-16 14:32:08 +0800

    modified connet.php

--- admin/connect.php
+++ admin/connect.php
@@ -2,16 +2,6 @@
     header("Content-type:text/html;charset:utf-8");
     date_default_timezone_set('Asia/Shanghai'); 
     define('HOST','localhost');
-
-    // define('USERNAME','nieyeni_patrol');
-    // define('PASSWORD','iampatrol');
-    // if(!($con=mysql_connect(HOST,USERNAME,PASSWORD))){
-    //     echo mysql_error(); 
-    // }
-    // if(!mysql_select_db('nieyeni_patrol')){
-    //     echo mysql_error(); 
-    // }
-
     define('USERNAME','root');
     define('PASSWORD','123456');
     if(!($con=mysql_connect(HOST,USERNAME,PASSWORD))){
